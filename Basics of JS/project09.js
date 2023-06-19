281150ff10e38831e351ba1a6321a8942e27eb7d
Chandan Singh <chandansinghcs9536@gmail.com>
2023-06-19 19:54:21 +0530

    h

--- Basics of JS/project09.js
+++ Basics of JS/project09.js
@@ -3,7 +3,7 @@
 let string1 = "17";
 let string2 = "10";
 
-// string ke aage +laga do 
+// string ke aage + laga do 
 let newString = +string1 + +string2;
 console.log(typeof newString);
 console.log(newString);
